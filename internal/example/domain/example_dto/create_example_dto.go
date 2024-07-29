c41779903033e84674d9534af1bed523137238d2
charmingruby <mrrubycanal3105@gmail.com>
2024-07-28 22:41:49 -0300

    feat: cleanup examples

--- internal/example/domain/example_dto/create_example_dto.go
+++
@@ -1,5 +0,0 @@
-package example_dto
-
-type CreateExampleUseCaseDTO struct {
-	Name string `json:"name"`
-}
--- internal/example/domain/example_repository/example_repository.go
+++
@@ -1,8 +0,0 @@
-package example_repository
-
-import "github.com/charmingruby/swrc/internal/example/domain/example_entity"
-
-type ExampleRepository interface {
-	Store(e *example_entity.Example) error
-	FindByID(id string) (*example_entity.Example, error)
-}

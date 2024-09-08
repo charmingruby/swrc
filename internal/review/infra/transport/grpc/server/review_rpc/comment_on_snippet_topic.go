437faa7722605aa34ef45b2e508b490f862eab7f
charmingruby <mrrubycanal3105@gmail.com>
2024-09-08 09:05:46 -0300

    feat: comment on snippet topic grpc handler impl

---
+++ internal/review/infra/transport/grpc/server/review_rpc/comment_on_snippet_topic.go
@@ -0,0 +1,45 @@
+package review_rpc
+
+import (
+	"context"
+	"errors"
+
+	"github.com/charmingruby/swrc/internal/common/core"
+	"github.com/charmingruby/swrc/internal/common/infra/transport/grpc"
+	"github.com/charmingruby/swrc/internal/common/infra/transport/grpc/interceptor"
+	"github.com/charmingruby/swrc/internal/review/domain/dto"
+	"github.com/charmingruby/swrc/proto/pb"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+func (h *ReviewGRPCService) CommentOnSnippetTopic(ctx context.Context, req *pb.CommentOnSnippetTopicRequest) (*emptypb.Empty, error) {
+	accountID := ctx.Value(interceptor.AccountIDKey).(string)
+	if accountID == "" {
+		return nil, grpc.NewInternalErr(errors.New("account id not found"))
+	}
+
+	input := dto.CommentOnSnippetTopicInputDTO{
+		Content:         req.Content,
+		SnippetTopicID:  req.SnippetTopicId,
+		ParentCommentID: req.ParentCommentId,
+		AccountID:       accountID,
+	}
+
+	if err := h.reviewService.CommentOnSnippetTopicUseCase(input); err != nil {
+		if notFoundErr, ok := err.(*core.ErrNotFound); ok {
+			return nil, grpc.NewNotFoundErr(notFoundErr)
+		}
+
+		if validationErr, ok := err.(*core.ErrValidation); ok {
+			return nil, grpc.NewValidationErr(validationErr)
+		}
+
+		if unauthorizedErr, ok := err.(*core.ErrUnauthorized); ok {
+			return nil, grpc.NewUnauthorizedErr(unauthorizedErr)
+		}
+
+		return nil, grpc.NewInternalErr(err)
+	}
+
+	return nil, nil
+}
